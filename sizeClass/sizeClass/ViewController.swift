00d493dc215b246caf63b6e9627b5b9c8c318c14
Saimassay <37999471+Saimassay@users.noreply.github.com>
2020-08-08 20:24:20 +0600

    Add files via upload

---
+++ sizeClass/sizeClass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sizeClass
+//
+//  Created by Gulnara Saimassay on 6/19/20.
+//  Copyright © 2020 Gulnara Saimassay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
